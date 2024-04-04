0712e28f134cd7b1f61ecdfa5259f1a843590511
akshim1702 <akshayshrivastava1702@gmail.com>
2024-04-04 14:01:46 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,63 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export const registerUser = asyncHandler(async ( req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: "User registered successfully",
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+export const registerUser = asyncHandler(async (req, res, next) => {
+  // get user details from frontend
+  // validation - not empty
+  // check if already exsist username,email
+  // check for images
+  // check for avatar
+  // upload them to cloudinary , avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return response else error
+
+  const { fullname, email, username, password } = req.body;
+
+  if (
+    [fullname, email, username, password].some((item) => item?.trim() === "")
+  ) {
+    throw new ApiError("All fields are required", 400);
+  }
+
+  const exsistedUser = User.findOne({ $or: [{ email }, { username }] });
+  if (exsistedUser) {
+    throw new ApiError("User already exists", 409);
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) throw new ApiError("Avtar file is required", 400);
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) throw new ApiError("Avtar file is required", 400);
+
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    email,
+    username: username.toLowerCase(),
+    password,
+    coverImage: coverImage?.url || "",
   });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError("Something went wrong when creating a new user", 500);
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
